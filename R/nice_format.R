1406a58f783f984a0d20461715ab6befacb0a842
Aleksander Dietrichson <dietrichson@gmail.com>
2019-01-28 13:05:49 -0300

    Added function for nice date-formatting

--- R/nice_format.R
+++ R/nice_format.R
@@ -1,5 +1,9 @@
 #' @title  Nice format
 #' @export
 .mf <- function(x){
-format(x,big.mark = ",")
+  if(is.numeric(x)) return(format(x,big.mark = ","))
+  myDate <- tryCatch(as.Date(x), error=function(x)x)
+  if(inherits(myDate, 'Date')) return(format(as.Date(x),"%b %d %Y"))
+  #If not found just return string
+  x
 }
