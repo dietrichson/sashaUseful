7f2889b4d61eae7d15d66ca9b509a718960ed349
Aleksander Dietrichson <dietrichson@gmail.com>
2019-02-24 15:20:17 -0300

    removed scales dependency -- it failed

--- R/format_data_frame_percentage.R
+++ R/format_data_frame_percentage.R
@@ -9,10 +9,11 @@
 #' @return a formatted data.frame
 #' @export
 format_data_frame_percentage <- function(df, na.string = ''){
-  tmp <- df %>% mutate_if(is.numeric,scales::percent)
+  # tmp <- df %>% mutate_if(is.numeric,scales::percent) #Breaks in some circumstances
+  tmp <- df %>% mutate_if(is.numeric,function(x){ sprintf('%s%%',x*100)})
   tmp <-  lapply(tmp, gsub, pattern = "NA%",  replacement =  na.string, fixed = TRUE)
   tmp <-  lapply(tmp, gsub, pattern = "NaN%", replacement =  na.string, fixed = TRUE)
   tmp <- as.data.frame(tmp)
-  names(tmp) <- names(df)
+   #names(tmp) <- names(df)
   tmp
 }
