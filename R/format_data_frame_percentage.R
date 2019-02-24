8fee493d9812c4942ea13c84394e53d47aa9ec1d
Aleksander Dietrichson <dietrichson@gmail.com>
2019-02-24 16:52:03 -0300

    Simplified, used only dplyr functions

--- R/format_data_frame_percentage.R
+++ R/format_data_frame_percentage.R
@@ -1,6 +1,7 @@
 #' Format data.frame with Percentages
 #'
-#' Will format all numbers in a data.frame as percentage. And replace NA with string specified in *replace* parameter.
+#' Will format all numbers in a data.frame as percentage. 
+#' And replace NA with string specified in *na.string* parameter.
 #' 
 #' 
 #' @param df the data.frame to format
@@ -9,11 +10,7 @@
 #' @return a formatted data.frame
 #' @export
 format_data_frame_percentage <- function(df, na.string = ''){
-  # tmp <- df %>% mutate_if(is.numeric,scales::percent) #Breaks in some circumstances
-  tmp <- df %>% mutate_if(is.numeric,function(x){ sprintf('%s%%',x*100)})
-  tmp <-  lapply(tmp, gsub, pattern = "NA%",  replacement =  na.string, fixed = TRUE)
-  tmp <-  lapply(tmp, gsub, pattern = "NaN%", replacement =  na.string, fixed = TRUE)
-  tmp <- as.data.frame(tmp)
-   #names(tmp) <- names(df)
-  tmp
+ df %>% mutate_if(is.numeric,function(x){ sprintf('%s%%',x*100)}) %>% 
+    mutate_all(function(x){stringr::str_replace(x,'(NA%|NaN%)',na.string)})
 }
+  
