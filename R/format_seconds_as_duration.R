7ef640d3d97a4b0cc9482a41ec4630f74c5d339e
Aleksander Dietrichson <dietrichson@gmail.com>
2019-08-22 14:55:58 -0300

    Added examples to documentation

--- R/format_seconds_as_duration.R
+++ R/format_seconds_as_duration.R
@@ -5,6 +5,11 @@
 #' @importFrom lubridate seconds_to_period
 #' @return a vector of characters
 #' @export
+#' 
+#' @examples 
+#' seconds_as_duration(c(100,200,3024, 16254))
+#' seconds_as_duration(c(100,200,3024, 16254), format=NULL)
+#' seconds_as_duration(c(100,200,3024, 16254), format='%02dh%02dm%02ds')
 seconds_as_duration<- function(x, format = '%02d:%02d:%02d' ){
   x <- as.numeric(x)
   if(!is.numeric(x) )stop("Numeric Vector needed") 
